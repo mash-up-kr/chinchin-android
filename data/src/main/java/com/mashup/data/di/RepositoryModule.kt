0f36f86b1c893309bd6cdd323c2078bfc2d9422c
KM <sodp2@naver.com>
2022-07-05 00:40:56 +0900

    feat: #7 jwt 로컬에 저장, 불러오기 추가

--- data/src/main/java/com/mashup/data/di/RepositoryModule.kt
+++ data/src/main/java/com/mashup/data/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.mashup.data.di
 
 import com.mashup.data.repository.GithubRepository
+import com.mashup.data.repository.LoginRepository
 import com.mashup.data.repository.impl.GithubRepositoryImpl
+import com.mashup.data.repository.impl.LoginRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,4 +16,8 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun bindGithubRepository(githubRepositoryImpl: GithubRepositoryImpl): GithubRepository
+
+    @Binds
+    @Singleton
+    fun bindLoginRepository(loginRepositoryImpl: LoginRepositoryImpl): LoginRepository
 }
