9d34e2d3538d3a6c7942153b2690b9563f55647e
hj1115hj <hyejin_one@naver.com>
2022-07-05 21:32:38 +0900

    feature/#6-Create MainActivity

---
+++ presenter/src/main/java/com/mashup/presenter/ui/main/BottomBarScreen.kt
@@ -0,0 +1,29 @@
+package com.mashup.presenter.ui.main
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomBarScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+){
+    object Home: BottomBarScreen(
+        route = "home",
+        title = "Home",
+        icon = Icons.Default.Home
+    )
+    object Profile: BottomBarScreen(
+        route = "Profile",
+        title = "Profile",
+        icon = Icons.Default.Person
+    )
+    object Setting: BottomBarScreen(
+        route = "setting",
+        title = "Setting",
+        icon = Icons.Default.Settings
+    )
+}
---
+++ presenter/src/main/java/com/mashup/presenter/ui/main/screens/HomeScreen.kt
@@ -0,0 +1,25 @@
+package com.mashup.presenter.ui.main.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.constraintlayout.compose.ConstraintLayout
+
+
+@Composable
+fun HomeScreen() {
+    ConstraintLayout(modifier = Modifier.background(Color.Cyan)) {
+
+        val text = createRef()
+        Text("Profile", Modifier.constrainAs(text) {
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        })
+    }
+}
+
+
