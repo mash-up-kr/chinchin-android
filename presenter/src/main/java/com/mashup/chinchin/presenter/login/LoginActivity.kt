9b0f8126620232695a99f061331fa30851851d61
AnKyungMoo <sodp2@naver.com>
2022-08-16 21:33:58 +0900

    feat: #75 로그인 ui 변경

--- presenter/src/main/java/com/mashup/chinchin/presenter/login/LoginActivity.kt
+++ presenter/src/main/java/com/mashup/chinchin/presenter/login/LoginActivity.kt
@@ -5,17 +5,22 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kakao.sdk.user.UserApiClient
+import com.mashup.chinchin.presenter.R
 import com.mashup.chinchin.presenter.ui.login.IntroductionImage
 import com.mashup.chinchin.presenter.ui.login.IntroductionText
 import com.mashup.chinchin.presenter.ui.login.KakaoLoginButton
 import com.mashup.chinchin.presenter.ui.theme.ChinchinTheme
+import com.mashup.chinchin.presenter.ui.theme.Secondary_1
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -57,31 +62,36 @@ class LoginActivity : ComponentActivity() {
 
 @Composable
 private fun LoginScreen(kakaoLogin: () -> Unit = {}) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-            .padding(vertical = 80.dp)
+    Box(
+        modifier = Modifier.background(Secondary_1)
     ) {
-        IntroductionText()
-        Spacer(modifier = Modifier.height(30.dp))
-        IntroductionImage()
-    }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(vertical = 153.dp),
+        ) {
+            Image(painter = painterResource(id = R.drawable.login_logo), contentDescription = "")
+            IntroductionText()
+            Spacer(modifier = Modifier.height(34.dp))
+            IntroductionImage()
+        }
 
-    Column(
-        verticalArrangement = Arrangement.Bottom,
-        modifier = Modifier
-            .fillMaxHeight()
-            .padding(bottom = 32.dp)
-    ) {
-        KakaoLoginButton {
-            kakaoLogin()
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(bottom = 32.dp)
+        ) {
+            KakaoLoginButton {
+                kakaoLogin()
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun LoginPagePreview() {
     ChinchinTheme {
