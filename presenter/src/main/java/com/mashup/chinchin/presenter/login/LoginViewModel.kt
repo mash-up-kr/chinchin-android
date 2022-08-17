c6fcff5d03515acfd7b87578c97db52cf43f9d4f
AnKyungMoo <sodp2@naver.com>
2022-08-18 00:44:43 +0900

    refactor: #75 errormessage 관련 접근제한자 설정

--- presenter/src/main/java/com/mashup/chinchin/presenter/login/LoginViewModel.kt
+++ presenter/src/main/java/com/mashup/chinchin/presenter/login/LoginViewModel.kt
@@ -21,7 +21,9 @@ class LoginViewModel @Inject constructor(
     val isLoginSuccess: LiveData<Boolean>
         get() = _isLoginSuccess
 
-    val errorMessage: MutableLiveData<String> = MutableLiveData()
+    private val _errorMessage: MutableLiveData<String> = MutableLiveData()
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
 
     fun handleKakaoCallback(token: OAuthToken?, error: Throwable?) {
         error?.let {
@@ -35,9 +37,9 @@ class LoginViewModel @Inject constructor(
 
     private fun handleError(error: Throwable) {
         if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-            errorMessage.value = "사용자가 명시적으로 취소"
+            _errorMessage.value = "사용자가 명시적으로 취소"
         } else {
-            errorMessage.value = "인증 에러 ${error.message}"
+            _errorMessage.value = "인증 에러 ${error.message}"
         }
     }
 
