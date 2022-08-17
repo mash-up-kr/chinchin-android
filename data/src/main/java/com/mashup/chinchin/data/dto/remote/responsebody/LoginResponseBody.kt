067979a1df7ae85f5a9a7170fc5e3266f6760c88
AnKyungMoo <sodp2@naver.com>
2022-08-18 00:34:42 +0900

    feat: #75 dto 필드명 변경

--- data/src/main/java/com/mashup/chinchin/data/dto/remote/responsebody/LoginResponseBody.kt
+++ data/src/main/java/com/mashup/chinchin/data/dto/remote/responsebody/LoginResponseBody.kt
@@ -3,5 +3,5 @@ package com.mashup.chinchin.data.dto.remote.responsebody
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponseBody(
-    @SerializedName("accessToken") val jwt: String?
+    @SerializedName("jwt") val jwt: String?
 )
