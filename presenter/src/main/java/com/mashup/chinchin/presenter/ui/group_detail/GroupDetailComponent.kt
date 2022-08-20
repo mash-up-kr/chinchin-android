d14d755c016ecc4b7eed3b1b5ecd055bc6f269c6
huijiny <jiin20803@gmail.com>
2022-08-20 22:59:30 +0900

    feat: #86 리베이스 유실 코드 추가

--- presenter/src/main/java/com/mashup/chinchin/presenter/ui/group_detail/GroupDetailComponent.kt
+++ presenter/src/main/java/com/mashup/chinchin/presenter/ui/group_detail/GroupDetailComponent.kt
@@ -1,41 +1,24 @@
 package com.mashup.chinchin.presenter.ui.group_detail
 
-import android.content.Context
-import android.content.Intent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import com.mashup.chinchin.presenter.friend_detail.FriendDetailActivity
-import com.mashup.chinchin.presenter.group_detail.model.GroupDetailUiModel
 import com.mashup.chinchin.presenter.main.model.FriendUiModel
 import com.mashup.chinchin.presenter.ui.common.ChinChinFriendCard
-import com.mashup.chinchin.presenter.ui.theme.Black
-import com.mashup.chinchin.presenter.ui.theme.Secondary_1
 
 @Composable
-fun GroupDetailList(context: Context, friends: List<FriendUiModel>, modifier: Modifier = Modifier) {
+fun GroupDetailList(
+    onClickCard: (FriendUiModel) -> Unit = {},
+    friends: List<FriendUiModel>,
+    modifier: Modifier = Modifier
+) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier
@@ -47,6 +30,7 @@ fun GroupDetailList(context: Context, friends: List<FriendUiModel>, modifier: Mo
                     .height(78.dp)
                     .padding(horizontal = 24.dp),
                 friend = friend,
+                onClickCard = { onClickCard(friend) }
             )
         }
     }
@@ -61,5 +45,6 @@ fun GroupDetailItemPreview() {
             .height(78.dp)
             .padding(horizontal = 24.dp),
         friend = FriendUiModel(0, "김매쉬", ""),
+        onClickCard = {}
     )
 }
