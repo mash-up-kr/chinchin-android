a32d3ad2be6d519a388496acf445f502e2bef5c2
AnKyungMoo <sodp2@naver.com>
2022-08-22 20:09:34 +0900

    feat: #59 테스트용 uiModel 타입 분기

--- presenter/src/main/java/com/mashup/chinchin/presenter/receive_alarm/ReceiveAlarmActivity.kt
+++ presenter/src/main/java/com/mashup/chinchin/presenter/receive_alarm/ReceiveAlarmActivity.kt
@@ -40,7 +40,7 @@ class ReceiveAlarmActivity : ComponentActivity() {
                     requestUserName = "경무",
                     requestUserProfileUrl = "https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_500.jpg?61e575e8653e5920470a38d1482d7312/melon/resize/416/quality/80/optimize",
                     requestDate = index.toLong(),
-                    alarmType = AlarmType.REQUEST,
+                    alarmType = if (index % 2 == 0) AlarmType.REQUEST else AlarmType.REPLY,
                 )
             )
         }
