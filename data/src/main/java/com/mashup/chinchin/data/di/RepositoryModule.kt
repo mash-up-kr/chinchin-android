ed32946ea5e12ec8d7853c6889d676f1d407203e
AnKyungMoo <sodp2@naver.com>
2022-08-27 01:38:13 +0900

    feat: #119 추천친구repo 힐트에 바인딩

--- data/src/main/java/com/mashup/chinchin/data/di/RepositoryModule.kt
+++ data/src/main/java/com/mashup/chinchin/data/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.mashup.chinchin.data.di
 
 import com.mashup.chinchin.data.repository.GroupRepositoryImpl
 import com.mashup.chinchin.data.repository.LoginRepositoryImpl
+import com.mashup.chinchin.data.repository.RecommendedFriendRepositoryImpl
 import com.mashup.chinchin.domain.repository.GroupRepository
 import com.mashup.chinchin.domain.repository.LoginRepository
+import com.mashup.chinchin.domain.repository.RecommendedFriendRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -20,4 +22,10 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun bindGroupRepository(groupRepositoryImpl: GroupRepositoryImpl): GroupRepository
+
+    @Binds
+    @Singleton
+    fun bindRecommendedFriendRepository(
+        recommendedFriendRepositoryImpl: RecommendedFriendRepositoryImpl,
+    ): RecommendedFriendRepository
 }
