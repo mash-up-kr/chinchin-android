ecd4bf967b14ebf21d5057abe7c7a4eac381623f
AnKyungMoo <sodp2@naver.com>
2022-08-30 23:02:22 +0900

    feat: #128 임시 커밋..ㅠㅠ

--- presenter/src/main/java/com/mashup/chinchin/presenter/receive_alarm/ReceiveAlarmActivity.kt
+++ presenter/src/main/java/com/mashup/chinchin/presenter/receive_alarm/ReceiveAlarmActivity.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.mashup.chinchin.presenter.receive_alarm.model.AlarmType
 import com.mashup.chinchin.presenter.receive_alarm.model.RequestAlarmUiModel
 import com.mashup.chinchin.presenter.ui.common.ChinChinToolbar
@@ -26,27 +28,11 @@ class ReceiveAlarmActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ChinchinTheme {
-                ReceiveAlarmScreen(requestAlarmUiModels = initRequestAlarms()) {
+                ReceiveAlarmScreen {
                     finish()
                 }
             }
         }
-    }
-
-    private fun initRequestAlarms(): List<RequestAlarmUiModel> {
-        val requestAlarmUiModels = mutableListOf<RequestAlarmUiModel>()
-        repeat(20) { index ->
-            requestAlarmUiModels.add(
-                RequestAlarmUiModel(
-                    requestUserName = "경무",
-                    requestUserProfileUrl = "https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_500.jpg?61e575e8653e5920470a38d1482d7312/melon/resize/416/quality/80/optimize",
-                    requestDate = index.toLong(),
-                    alarmType = if (index % 2 == 0) AlarmType.REQUEST else AlarmType.REPLY,
-                )
-            )
-        }
-
-        return requestAlarmUiModels.toList()
     }
 }
 
@@ -58,9 +44,12 @@ fun ReceiveAlarmPreview() {
 
 @Composable
 fun ReceiveAlarmScreen(
-    requestAlarmUiModels: List<RequestAlarmUiModel> = listOf(),
     finishActivity: () -> Unit = {},
 ) {
+    val viewModel: ReceiveAlarmViewModel = hiltViewModel()
+    viewModel.getAlarms()
+    val requestAlarmUiModels: List<RequestAlarmUiModel> = viewModel.alarms.observeAsState().value ?: emptyList()
+
     StatusBarColor()
     Column {
         ChinChinToolbar(
--- presenter/src/main/java/com/mashup/chinchin/presenter/receive_alarm/model/RequestAlarmUiModel.kt
+++ presenter/src/main/java/com/mashup/chinchin/presenter/receive_alarm/model/RequestAlarmUiModel.kt
@@ -1,8 +1,20 @@
 package com.mashup.chinchin.presenter.receive_alarm.model
 
+import com.mashup.chinchin.domain.model.Alarm
+
+/* TODO: 연중이가 고쳐주면 싹 바꿔야됨 이름도 바꾸자 */
 data class RequestAlarmUiModel(
     val requestUserName: String,
     val requestUserProfileUrl: String,
     val requestDate: Long,
     val alarmType: AlarmType,
 )
+
+fun Alarm.toUiModel(): RequestAlarmUiModel {
+    return RequestAlarmUiModel(
+        requestUserName = this.friendId.toString(),
+        requestUserProfileUrl = this.questionnaireId.toString(),
+        requestDate = this.createdAt,
+        alarmType = AlarmType.fromValue(this.type)
+    )
+}
