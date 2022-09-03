45665dea10d1c5e1aece47da4e21d4579288165b
AnKyungMoo <sodp2@naver.com>
2022-09-04 01:37:37 +0900

    feat: #165 코드리뷰 반영
    - 도메인에 common 관련을 login으로 합침

--- domain/src/main/java/com/mashup/chinchin/domain/usecase/SetShowCongratulationDialogUseCase.kt
+++ domain/src/main/java/com/mashup/chinchin/domain/usecase/SetShowCongratulationDialogUseCase.kt
@@ -1,11 +1,12 @@
 package com.mashup.chinchin.domain.usecase
 
+import com.mashup.chinchin.domain.repository.LoginRepository
 import javax.inject.Inject
 
 class SetShowCongratulationDialogUseCase @Inject constructor(
-    private val commonRepository: CommonRepository,
+    private val loginRepository: LoginRepository,
 ) {
     operator fun invoke(isShowAble: Boolean) {
-        commonRepository.setIsFirstEnter(isShowAble)
+        loginRepository.setIsFirstEnter(isShowAble)
     }
 }
